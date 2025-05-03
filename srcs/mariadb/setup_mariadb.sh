a71b9bb297659c35875dbc4f205672a5ffaa02fa
anmedyns <anmedyns@student.42roma.it>
2025-05-03 22:32:07 +0200

    wordpress_working

--- srcs/mariadb/setup_mariadb.sh
+++ srcs/mariadb/setup_mariadb.sh
@@ -1,29 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "_+_+_+_+📊 Starting MariaDB setup..._+_+_+_+"
+# Start the MariaDB service
+service mariadb start
 
-chown -R mysql:mysql /var/log/mysql
+sleep 10
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "###Initializing database..."
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-	mysql --user=mysql &
-	pid=$!
+# Create the database if it doesn't exist
+echo "Creating database if it doesn't exist..."
+mariadb  -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-	echo "###Waiting for database to be ready..."
-	sleep 15;
+# Create the user if it doesn't exist
+echo "Creating user if it doesn't exist..."
+mariadb  -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-	mysql -u root << EOF
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-	CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED  BY '${MYSQL_PASSWORD}';
-	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-	FLUSH PRIVILEGES;
-EOF
+# Grant all privileges to the user on the database
+echo "Granting privileges..."
+mariadb  -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' WITH GRANT OPTION;"
 
-	sleep 15;
-	
-	kill $pid
-	wait $pid
-fi
-exec mysqld --user=mysql
+# Change the root user's password
+echo "Changing root user's password..."
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+# Flush privileges to ensure changes take effect
+echo "Flushing privileges..."
+mariadb -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+
+# Shut down the mariadb service
+echo "Shutting down MariaDB service..."
+mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+# Wait for shutdown to complete
+sleep 10
+
+# Restart MariaDB in safe mode
+exec mysqld_safe
--- srcs/wordpress/conf/wp-config.sh
+++ srcs/wordpress/conf/wp-config.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 echo "Starting configuring WordPress..."
 
-wp core download --all-root
+sleep 10
+
+wp core download --allow-root
+
+sleep 22
 
 echo "Create wp-config.php..."
 wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_USER --dbpass=$WORDPRESS_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 
 wp core install --url="http://localhost" --title="anmedyns" --admin_user="admin" --admin_password="adminpassword" --admin_email="anmedyns@student.42roma.it" --allow-root
+sleep 10
 
 php-fpm82 --nodaemonize
+echo "Finish wp-config.php..."
+
 
